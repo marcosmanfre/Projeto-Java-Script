b36c5d51df980286b9faa08f1b6f125eca5206c9
Marcos Manfre <marcosmanfre@hotmail.com>
2022-02-05 10:48:22 -0300

    js_POO

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,30 @@
-
+import { Cliente } from "./Cliente.js"
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
     _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){        
+        this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this.cliente;
+    }
+
+    get saldo(){
+        return this.saldo;
+    }
+
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
--- index.js
+++ index.js
@@ -2,24 +2,20 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
 
-const ContaCorrenteRicardo = new ContaCorrente();
 
-ContaCorrenteRicardo.agencia = 1001;
-ContaCorrenteRicardo.cliente = cliente1;
+const cliente2 = new Cliente("Alice", 444455566);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
 
-console.log(conta2)
+const ContaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+
+
+const conta2 = new ContaCorrente(102, cliente2);
+
+
+console.log(ContaCorrenteRicardo);
 
 
 
