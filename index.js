71fc8a6c77f71db9031cf34ff1decb8bcd949458
Marcos Manfre <marcosmanfre@hotmail.com>
2022-02-18 10:27:03 -0300

    subir

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ import {ContaCorrente} from "./ContaCorrente.js"
 
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-
-
-
 const cliente2 = new Cliente("Alice", 444455566);
-
-
 const ContaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-
-
 const conta2 = new ContaCorrente(102, cliente2);
 
 
